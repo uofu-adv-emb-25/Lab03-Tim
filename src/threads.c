f86d9ef9ec987031f5a4a2efce1c945075f950df
Timothy Felt <timothyfelt@MacBookPro.hsd1.ut.comcast.net>
2025-11-10 20:02:59 -0700

    Committing threads with protected semaphores.

--- src/threads.c
+++ src/threads.c
@@ -21,8 +21,11 @@ void side_thread(void *params)
 {
 	while (1) {
         vTaskDelay(100);
+        if(xSemaphoreTake(semaphore, portMAX_DELAY) == pdTRUE){
+        printf("hello world from %s! Count %d\n", "thread", counter);
         counter += 1;
-		printf("hello world from %s! Count %d\n", "thread", counter);
+        xSemaphoreGive(semaphore);
+        }
 	}
 }
 
@@ -31,7 +34,10 @@ void main_thread(void *params)
 	while (1) {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
         vTaskDelay(100);
+        if(xSemaphoreTake(semaphore, portMAX_DELAY) == pdTRUE){
 		printf("hello world from %s! Count %d\n", "main", counter++);
+        xSemaphoreGive(semaphore);
+        }
         on = !on;
 	}
 }
